acf7a9d0a7e4158a88a248825c910fb1e797bad3
Abdoul Akhad Top <akhad0015@gmail.com>
2021-08-17 00:05:02 +0000

    complete pagination in home page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -14,8 +14,14 @@ def index(request):
     grades = Grade.objects.order_by('-grades')
     # grades = Grade.objects.order_by('-grades').filter(grades__gte=10)
 
+    grade_list = Grade.objects.order_by('-grades')
+    paginator = Paginator(grade_list, 3)  # Show 3 grades per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'grades': grades
+        # 'grades': grades,
+        'grades': page_obj
     }
     return render(request, 'pages/index.html', context)
 
